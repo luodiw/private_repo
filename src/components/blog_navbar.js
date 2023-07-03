1abdbaadb58381eb0c2fe1bf1aa6f4c6e82b44de
Luodi Wang <luodiwang4@gmail.com>
2023-07-02 21:45:44 -0400

    url

--- src/components/blog_navbar.js
+++ src/components/blog_navbar.js
@@ -20,7 +20,7 @@ function BlogNavBar() {
         <nobr>
           <NavLink to="/">home</NavLink> | <NavLink to="/about">about</NavLink>{" "}
           | <NavLink to="/projects">projects</NavLink> |{" "}
-          <NavLink to="/log">reading</NavLink> |{" "}
+          <NavLink to="/reading">reading</NavLink> |{" "}
           {/*<NavLink to="/resume">resume</NavLink> |{" "}*/}
           <CSSTransition
             in={visible}
