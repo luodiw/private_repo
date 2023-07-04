1ddd2ba753bb404032811ba4708a90e2e45fe992
Luodi Wang <luodiwang4@gmail.com>
2023-07-04 17:08:17 -0400

    more navbar naming

--- src/components/blog_navbar.js
+++ src/components/blog_navbar.js
@@ -19,7 +19,7 @@ function BlogNavBar() {
       <nav>
         <nobr>
           <NavLink to="/">home</NavLink> |
-          <NavLink to="/about">about</NavLink>{" "}
+          <NavLink to="/about">footprint</NavLink>{" "}
           | <NavLink to="/projects">projects</NavLink> |{" "}
           <NavLink to="/log">reading</NavLink> |{" "}
 
