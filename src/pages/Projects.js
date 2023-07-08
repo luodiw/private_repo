adde13eaf72fa4ae5c632e95739a0cb389a6cd92
Luodi Wang <luodiwang4@gmail.com>
2023-07-08 01:31:09 -0400

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -41,12 +41,39 @@ function Projects() {
         </a>{" "}
         <br />
         built an efficient Discord bot template for deployment and workflow with Docker, Pipenv, and autoloading Cogs
+        <br />
         deployed across servers totallling to 2K people  
         (gpt addition incoming ;))
       </p>
 
+      <p>
+        <a
+          href="https://github.com/luodiw/Instafarm-Web-Scrape"
+          class="clickable"
+          target="_blank"
+        >
+          <b>Instafarm Scraper</b>
+        </a>{" "}
+        <br />
+        used SerpApi's Google Maps API and bs4 to make a categorized, comprehensive nation wide database 
+        of your local, organic farms 
+      </p>
 
       <p>
+        <a
+          href="https://github.com/luodiw/Hasherator"
+          class="clickable"
+          target="_blank"
+        >
+          <b>Hasherator</b>
+        </a>{" "}
+        <br />
+        made a CLI that generates customized random hashes with Node.js 
+      </p>
+
+      <i>(work in progress, more projects incoming!)</i>
+
+      {/* <p>
         <NavLink to="/quinn-ai" class="clickable">
           <u>
             <b>Discord Bot Template</b>
@@ -136,7 +163,7 @@ function Projects() {
 
       <p>
         <i>(this page is a work in progress, pictures coming soon!)</i>
-      </p>
+      </p> */}
     </div>
   );
 }
