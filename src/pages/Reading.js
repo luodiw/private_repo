2c2c47923c5a5395e949af59f6d882906a379ee2
Luodi Wang <luodiwang4@gmail.com>
2024-04-10 12:11:05 -0400

    Update book reading page

--- src/pages/Reading.js
+++ src/pages/Reading.js
@@ -16,43 +16,21 @@ function Reading() {
       <img src="/james_turrell_skyspace.jpg" alt="it's me!" width="200" height="auto" />
 
       <p>
-        follow my journeys on{" "}
+        my {" "}
         <a href="https://curius.app/luodi-wang" target="_blank">
            <u>curius</u>
         </a>{" "}
       </p>
-      <p>
-          <b><h5> currently reading </h5> </b>
-      </p>
-      <p>
-          <ul>
-              <li> <i> Just for Fun </i>- Linus Torvalds</li>
-              <li> (diving deep into sci-fi currently)</li>
-
-          </ul>
-      </p>
       <p />
 
       <p>
-          <b><h5> books I read over and over again </h5> </b>
+          <b><h5> books I live by</h5> </b>
           <ul>
-              <li> <i> Meditations </i>- Marcus Aurelius</li>
-              <li> <i> The Lion Tracker's Guide to Life </i>- Boyd Varty</li>
-              {/* <li> <i> High Growth Handbook </i>- Elad Gil</li> */}
-              <li> <i> Can't Hurt Me </i>- David Goggins</li>
-              <li> <i> The Compound Effect </i>- Darren Hardy</li>
-              <li> <i> The Infinite Machine </i>- Camila Russo</li>
-              <li> <i> Aiming High A Biography of Masa Son </i>- Atsuo Inoue</li>
-              {/* <li> <i> Love Yourself Like Your Life Depends on It </i>- Kamal Ravikant</li> */}
-              <li> <i> After Steve </i>- Tripp Mickle</li>
-              <li><i> Zero to One </i>- Peter Thiel </li>
-              {/* <li><i> Be Here Now </i>- Ram Dass</li> */}
-              <li><i> The Almananck of Naval Ravikant </i></li>
-              <li><i> Business Adventures </i>- John Brooks </li>
-              {/* <li><i> Skin in the Game </i>- Nassim Nicholas Taleb </li> */}
-              <li><i> Infinite Powers</i>- Steven Strogatz</li>
-              <li><i> Twelve Against Gods </i>- William Bolitho</li>
-              <li><i> Amp It Up </i>- Frank Slootman </li>
+              <li> Zero: The Biography of a Dangerous Idea</li>
+              <li> Infinite Powers</li>
+              <li> Wanting: The Power of Mimetic Desire in Everyday Life</li>
+              <li> Can&apos;t Hurt Me</li>
+              <li> Make Something Wonderful</li>
           </ul>
       </p>
     </div>
