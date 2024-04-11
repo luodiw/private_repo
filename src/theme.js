fa81f5cdb2eff4572bf39536e4a5938f2c805f45
Luodi Wang <luodiwang4@gmail.com>
2024-04-11 16:54:36 -0400

    Undo theme.js deletion

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,33 @@
 // theme.js
 
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-// const useTheme = () => {
-//   const [theme, setTheme] = useState("sun");
+const useTheme = () => {
+  const [theme, setTheme] = useState("sun");
 
-//   useEffect(() => {
-//     const currentTheme = localStorage.getItem("theme");
-//     if (currentTheme) {
-//       setTheme(currentTheme);
-//     } else {
-//       const prefersDarkMode = window.matchMedia(
-//         "(prefers-color-scheme: dark)"
-//       ).matches;
-//       if (prefersDarkMode) {
-//         setTheme("moon");
-//       }
-//     }
-//   }, []);
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme) {
+      setTheme(currentTheme);
+    } else {
+      const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      if (prefersDarkMode) {
+        setTheme("moon");
+      }
+    }
+  }, []);
 
-//   const toggleTheme = () => {
-//     const newTheme = theme === "moon" ? "sun" : "moon";
-//     setTheme(newTheme);
-//     localStorage.setItem("theme", newTheme);
-//     document.documentElement.classList.toggle("sun");
-//     document.documentElement.classList.toggle("moon");
-//   };
+  const toggleTheme = () => {
+    const newTheme = theme === "moon" ? "sun" : "moon";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("sun");
+    document.documentElement.classList.toggle("moon");
+  };
 
-//   return [theme, toggleTheme];
-// };
+  return [theme, toggleTheme];
+};
 
-// export default useTheme;
+export default useTheme;
