4f66436336cd10aca3df3431c7bbdef9fcdb63dc
Luodi Wang <luodiwang4@gmail.com>
2024-04-11 11:48:07 -0400

    Footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -26,7 +26,7 @@ function Footer() {
           {" "}
         </a>
         <a
-           href= "mailto:lwang1289@gmail.com"
+           href= "mailto:lwang4319@gmail.com"
            target="_blank"
            // className="fa-solid fa-paper-plane"
            className="fa fa-paper-plane fa-sm"
@@ -34,46 +34,8 @@ function Footer() {
            {" "}
         </a>
         {"                                                                                    "}
-        <span className="love-nyc">made with ❤️ in nyc & sf</span>
-        {/* add a footer, made with love in nyc}
-        add time.js to this file
-
-          {/*<i className="fa-solid fa-paper-plane"></i>*/}
-        {/*<a*/}
-        {/*  href="https://www.instagram.com/qh.music/"*/}
-        {/*  target="_blank"*/}
-        {/*  class="fa fa-instagram fa-lg"*/}
-        {/*>*/}
-        {/*  {" "}*/}
-        {/*</a>*/}
-        {/*<a*/}
-        {/*  href="https://sesamebagel.substack.com/"*/}
-        {/*  target="_blank"*/}
-        {/*  class="fa fa-bookmark fa-lg"*/}
-        {/*>*/}
-        {/*  {" "}*/}
-        {/*</a>*/}
-        {/*<a*/}
-        {/*  href="https://bento.me/quinnha"*/}
-        {/*  target="_blank"*/}
-        {/*  class="	fa fa-caret-square-o-down fa-lg"*/}
-        {/*>*/}
-        {/*  {""}*/}
-        {/*</a>*/}
+        <span className="love-nyc">made with ❤️ in sf</span>
       </p>
-
-
-      {/* <p class="credit_p">
-        inspired by{" "}
-        <a
-          href="https://www.tiffanywang.me/"
-          target="_blank"
-          class="credit"
-        >
-          tiffany
-        </a>
-      </p> */}
-      {/* <Clock /> */}
     </div>
   );
 }
